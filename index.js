9e7f1baea904319ef290968a8274e9862f820ec8
Justine B <justinegayton@gmail.com>
2019-03-11 19:26:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function nestedTarget(nestedTarget){    //Nested = ID, Target = CLASS
  return document.querySelector('#nested .target');
 }
 
-function deepestChild(){  //deepest node in #grand-node
-  return document.querySelector('#grand-node');
+function deepestChild(){  //deepest node in #grand-node ('boo' 4 divs deep)
+  return document.querySelector('#grand-node .div .div .div .div');
 }
 
 function increaseRankBy(n){

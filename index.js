6ce1eab0e2122ab8a480722395f24ec9e079595b
Justine B <justinegayton@gmail.com>
2019-03-11 19:19:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(nestedTarget){    //Nested = ID, Target = CLASS
- let nested = document.queryElementById('nested');
- let target = nested.querySelectorAll('target');
- return target;
+ return document.querySelectorAll('#nested .target');
 }
